cc13305939c9fdc12016d3932a53de247454bf2c
Carlos Daniel Avila Navarro <carlosavila6274@gmail.com>
2021-03-17 00:40:53 +0000

    Solve aws-s3-explorer
    Signed-off-by: Carlos Daniel Avila Navarro <carlosavila6274@gmail.com>

--- labs/aws-s3-explorer/awsS3Client.go
+++ labs/aws-s3-explorer/awsS3Client.go
@@ -1,4 +1,96 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"encoding/json"
+)
+
+type BucketData struct{
+	BucketName string
+	DirectoryName string
+	ObjectsCount int
+	DirectoriesCount int
+	Extensions map[string]int
+}
+
+func printBucket(bucket BucketData){
+	var message = "{\n"
+	message = fmt.Sprintf(message + "	BucketName:		%s\n", bucket.BucketName)
+	if bucket.DirectoryName != ""{
+		message = fmt.Sprintf(message + "	DirectoryName:		%s\n", bucket.DirectoryName)
+	}
+	message = fmt.Sprintf(message + "	ObjectsCount:		%d\n", bucket.ObjectsCount)
+	message = fmt.Sprintf(message + "	DirectoriesCount:	%d\n", bucket.DirectoriesCount)
+	message = fmt.Sprintf(message + "	Extensions:{\n")
+	for extension,number := range bucket.Extensions{
+	message = fmt.Sprintf(message + "		%s:		%d\n", extension, number)
+	}
+	message = fmt.Sprintf(message + "	}\n")
+	message = fmt.Sprintf(message + "}\n")
+	fmt.Print(message)
+}
+
+func errorHandler(e error, i int){
+	switch i{
+	case 1:
+		fmt.Println("There was no bucket given\n")
+	case 2:
+		fmt.Println("Error creating connection: ", e)
+	case 3:
+		fmt.Println("Error connecting: ", e)
+	case 4:
+		fmt.Println("Error writing information: ", e)
+	case 5:
+		fmt.Println("Error reading information: ", e)
+	case 6:
+		fmt.Println("Cant access the bucket\n")
+	case 7:
+		fmt.Println("Error encoding information: ", e)
+	case 8:
+		fmt.Println("Error decoding information: ", e)
+	}
+}
+
+func handleConn(c net.Conn, m string) {
+	_,err := io.WriteString(c, m)
+	if err != nil{
+		errorHandler(err, 4)
+	}
+}
+
 func main() {
+	var proxy, bucket, directory string
+	flag.StringVar(&proxy, "proxy", "localhost:8000", "Connection to make")
+	flag.StringVar(&bucket, "bucket", "", "bucket to enter")
+	flag.StringVar(&directory, "directory", "", "directory to enter")
+	flag.Parse()
+	if bucket == ""{
+		errorHandler(nil, 1)
+		return
+	}
+	message := bucket
+	if directory != ""{
+		message += "/" + directory
+	}
+	conn, err := net.Dial("tcp", proxy)
+	if err != nil {
+		errorHandler(err, 3)
+	}
+	handleConn(conn, message)
+	var data BucketData
+	decoder := json.NewDecoder(conn)
+	err = decoder.Decode(&data)
+	if err != nil{
+		errorHandler(err, 8)
+	}
+	if data.BucketName == "-1"{
+		errorHandler(err, 6)
+		return
+	}
+	printBucket(data)
+	fmt.Print("\n")
+	conn.Close()
 }
--- labs/aws-s3-explorer/awsS3Explorer.go
+++ labs/aws-s3-explorer/awsS3Explorer.go
@@ -1,4 +1,158 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"encoding/json"
+	"golang.org/x/net/html"
+	"time"
+	"strings"
+	"path/filepath"
+)
+
+type BucketData struct{
+	BucketName string
+	DirectoryName string
+	ObjectsCount int
+	DirectoriesCount int
+	Extensions map[string]int
+}
+
+func errorHandler(e error, i int){
+	switch i{
+	case 1:
+		fmt.Println("There was no bucket given")
+	case 2:
+		fmt.Println("Error creating connection: ", e)
+	case 3:
+		fmt.Println("Error connecting: ", e)
+	case 4:
+		fmt.Println("Error writing information: ", e)
+	case 5:
+		fmt.Println("Error reading information: ", e)
+	case 6:
+		fmt.Println("Cant access the bucket")
+	case 7:
+		fmt.Println("Error encoding information: ", e)
+	case 8:
+		fmt.Println("Error decoding information: ", e)
+	}
+}
+
+func getData(c net.Conn) string{
+	message := ""
+	buffer := make([]byte, 1024)
+	bytes, err := c.Read(buffer)
+	if err != nil{
+		errorHandler(err, 5)
+	}
+	if bytes > 0{
+		message = string(buffer[:bytes])
+	}
+	return message
+}
+
+func transformData(name string) BucketData{
+	var bucketName, directoryName string
+	if strings.Contains(name, "/"){
+		split := strings.Split(name, "/")
+		bucketName = split[0]
+		directoryName = split[1]
+	} else{
+		bucketName = name
+		directoryName = ""
+	}
+	bucket := BucketData{bucketName, directoryName, -1, -1, make(map[string]int)}
+	visited := make(map[string]int)
+	url := "http://s3.amazonaws.com/" + bucketName
+	data, err := http.Get(url)
+	if err != nil{
+		errorHandler(err, 6)
+		return BucketData {"-1", "", 0, 0, make(map[string]int)}
+	}
+	tokenizer := html.NewTokenizer(data.Body)
+	for {
+		next := tokenizer.Next()
+		token := tokenizer.Token()
+		err := tokenizer.Err()
+		if err == io.EOF {
+			break
+		}
+		switch next {
+		case html.ErrorToken:
+			continue
+		case html.TextToken:
+			dateLayout := "2017-06-30T13:36:23.000Z"
+			_, err := time.Parse(dateLayout, token.Data)
+			if err != nil {
+				if directoryName == ""{
+					extension := filepath.Ext(token.Data)
+					if len(extension) > 0 {
+						extension = extension[1:]
+						if _, ext := bucket.Extensions[extension]; ext {
+							bucket.Extensions[extension] += 1
+						} else {
+							bucket.Extensions[extension] = 1
+						}
+					}
+					directory := filepath.Dir(token.Data)
+					if len(directory) > 1 && directory != ".." {
+						bucket.ObjectsCount += 1
+						if _,in := visited[directory]; !in{
+							visited[directory] = 1
+							bucket.DirectoriesCount += 1
+						}
+					}
+				}else{
+					directory := filepath.Dir(token.Data)
+					if len(directory) > 1 && directory != ".." && strings.Contains(directory, directoryName){
+						bucket.ObjectsCount += 1
+						if _,in := visited[directory]; !in{
+							visited[directory] = 1
+							bucket.DirectoriesCount += 1
+						}
+					}else{
+						continue
+					}
+					extension := filepath.Ext(token.Data)
+					if len(extension) > 0 {
+						extension = extension[1:]
+						if _, ext := bucket.Extensions[extension]; ext {
+							bucket.Extensions[extension] += 1
+						} else {
+							bucket.Extensions[extension] = 1
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return bucket
+}
+
 func main() {
+	var port string	
+	flag.StringVar(&port, "port", "", "port used")
+	flag.Parse()
+	listener, err := net.Listen("tcp", "localhost:"+port)
+	if err != nil {
+		errorHandler(err, 2)
+	}
+	for{
+		conn, err := listener.Accept()
+		if err != nil {
+			errorHandler(err, 3)
+		}
+		info := getData(conn)
+		encoder := json.NewEncoder(conn)
+		data := transformData(info)
+		err2 := encoder.Encode(data)
+		if err2 != nil{
+			errorHandler(err, 7)
+		}
+	}
 }
