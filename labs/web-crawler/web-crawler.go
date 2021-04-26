5ede6eb7e6c7d2dd58a27668556ddcea58ca3526
Carlos Daniel Avila Navarro <carlosavila6274@gmail.com>
2021-04-26 02:55:39 +0000

    Solve web-crawler
    Signed-off-by: Carlos Daniel Avila Navarro <carlosavila6274@gmail.com>

--- labs/web-crawler/web-crawler.go
+++ labs/web-crawler/web-crawler.go
@@ -14,8 +14,9 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
+	"flag"
+	"strconv"
 
 	"gopl.io/ch5/links"
 )
@@ -25,43 +26,108 @@ import (
 // enforce a limit of 20 concurrent requests.
 var tokens = make(chan struct{}, 20)
 
-func crawl(url string) []string {
-	fmt.Println(url)
+func errorHandler(c int, e error, l int){
+	switch(c){
+		case 1:
+			fmt.Println("Please enter a limit bigger than " + strconv.Itoa(l))
+			break
+		case 2:
+			fmt.Println("Please enter a name for the file")
+			break
+		case 3:
+			fmt.Println("Please enter a URL to crawl through")
+			break
+		case 4:
+			fmt.Println("There was an error while crawling in depth " + strconv.Itoa(l) + ": " + e.Error())
+			break
+		case 5:
+			fmt.Println("There was an error while creating the file: " + e.Error())
+			break
+		case 6: 
+			fmt.Println("There was an error while writing in the file: " + e.Error())
+			break
+	}
+}
+
+func createFile(name string, links []string){
+	file, err := os.Create(name)
+    if err != nil {
+		errorHandler(5, err, 0)
+		return
+    }
+	defer file.Close()
+	var text string
+	for _, link := range links{
+		text += link
+		text += "\n" 
+	}
+	_, err = file.WriteString(text)
+	if err != nil {
+		errorHandler(6, err, 0)
+		return
+	}
+}
+
+func crawl(url string, depth int) []string {
 	tokens <- struct{}{} // acquire a token
 	list, err := links.Extract(url)
 	<-tokens // release the token
-
 	if err != nil {
-		log.Print(err)
+		errorHandler(4, err, depth)
+		return make([]string, 0)
 	}
 	return list
+}
+
+func depthLimiter(url string, depthLimit int, depth int, visited map[string]bool) map[string]bool{
+	visited[url] = true
+	if depth < depthLimit{
+		list := crawl(url, depth)
+		for _, link := range list{
+			if !visited[link]{
+				visited = depthLimiter(link, depthLimit, depth+1, visited)
+			}
+		}
+	}
+	return visited
+}
+
+func getKeys(dict map[string]bool) []string{
+	keys := make([]string, 0, len(dict))
+	for key := range dict{
+		keys = append(keys, key)
+	}
+	return keys
 }
 
 //!-sema
 
 //!+
 func main() {
-	worklist := make(chan []string)
-	var n int // number of pending sends to worklist
+	var limit int
+	var result string
+	flag.IntVar(&limit, "depth", 0, "Depth of the web-crawler")
+	flag.StringVar(&result, "results", "", "Name of file")
+	flag.Parse()
 
-	// Start with the command-line arguments.
-	n++
-	go func() { worklist <- os.Args[1:] }()
-
-	// Crawl the web concurrently.
-	seen := make(map[string]bool)
-	for ; n > 0; n-- {
-		list := <-worklist
-		for _, link := range list {
-			if !seen[link] {
-				seen[link] = true
-				n++
-				go func(link string) {
-					worklist <- crawl(link)
-				}(link)
-			}
-		}
+	if limit < 1{
+		errorHandler(1, nil, limit)
+		return
 	}
+	if result == ""{
+		errorHandler(2, nil, 0)
+		return
+	}
+	if len(flag.Args()) == 0{
+		errorHandler(3, nil, 0)
+		return
+	}
+	url := flag.Args()[0]
+
+	seen := make(map[string]bool)
+	links := getKeys(depthLimiter(url, limit, 0, seen))
+	createFile(result, links)
+	fmt.Println("The crawling has ended, please check " + result + " to see the results.")
 }
 
 //!-
